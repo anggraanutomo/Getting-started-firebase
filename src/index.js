85a3c7456110019a328752190a48efa03c10a0b4
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2023-02-02 18:54:31 +0700

    Update document

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {
     getFirestore, collection, onSnapshot,
     getDocs, addDoc, deleteDoc, doc,
     query, where, orderBy, serverTimestamp,
-    getDoc
+    getDoc, updateDoc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -73,3 +73,17 @@ onSnapshot(docRef, (doc) => {
     console.log(doc.data(), doc.id)
 })
 
+// updating a document
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let docRef = doc(db, 'books', updateForm.id.value)
+
+    updateDoc(docRef, {
+        title: 'updated title'
+    })
+        .then(() => {
+            updateForm.reset()
+        })
+})

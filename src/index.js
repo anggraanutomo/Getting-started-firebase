ff1991794ba87bf8bbf7190fa2aa5c553888a8f6
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2023-02-02 18:21:39 +0700

    Adding & Deleting Documents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app'
 import {
-    getFirestore, collection, getDocs
+    getFirestore, collection, getDocs, addDoc, deleteDoc, doc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -33,3 +33,31 @@ getDocs(colRef)
     .catch(err => {
         console.log(err.message)
     })
+
+// adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        title: addBookForm.title.value,
+        author: addBookForm.author.value
+    })
+        .then(() => {
+            addBookForm.reset()
+        })
+})
+
+// deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+    deleteDoc(docRef)
+        .then(() => {
+            deleteBookForm.reset()
+        })
+
+})

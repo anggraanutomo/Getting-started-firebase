a0d64cc8ec4989a3f1971160a6ebd6b9dec0d51a
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2023-02-02 18:05:12 +0700

    Firestore Setup & Fetching Data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { initializeApp } from 'firebase/app'
+import {initializeApp} from 'firebase/app'
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBN-WhJ9BfJ4wk1aCed9htLE2Gva1Xlj9o",
@@ -9,4 +12,24 @@ const firebaseConfig = {
     appId: "1:18598714807:web:83814bf3ec1c4ca7ec1c32"
 };
 
+// init firebase app
 initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({...doc.data(), id: doc.id})
+        })
+        console.log(books)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
